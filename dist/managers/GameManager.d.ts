aa3c99594686ba1f318a0620c6e1315d18f3b122
SakuraCase <sakurada6617@gmail.com>
2020-08-23 18:20:36 +0000

    dist

---
+++ dist/managers/GameManager.d.ts
@@ -0,0 +1,68 @@
+import * as PIXI from "pixi.js";
+import ApplicationOptions from 'interfaces/ApplicationOptions';
+import Scene from 'scenes/Scence';
+/**
+ * ゲーム全体のマネージャ
+ * 主にシーンを扱う
+ */
+export default class GameManager {
+    /**
+     * シングルトン新スタンス
+     */
+    static instance: GameManager;
+    /**
+     * PIXI.Application インスタンス
+     * 初期化時に生成される
+     */
+    game: PIXI.Application;
+    /**
+     * シーンのリソースロード完了フラグ
+     * シーントランジションを制御するためのフラグ
+     */
+    private sceneResourceLoaded;
+    /**
+     * シーンのトランジション完了フラグ
+     * シーントランジションを制御するためのフラグ
+     */
+    private sceneTransitionOutFinished;
+    /**
+     * 現在のシーンインスタンス
+     */
+    private currentScene?;
+    /**
+     * コンストラクタ
+     * PIXI.Application インスタンスはユーザ任意のものを使用する
+     */
+    constructor(app: PIXI.Application);
+    /**
+     * ゲームを起動する
+     * 画面サイズや ApplicationOptions を渡すことができる
+     */
+    static start(params: {
+        glWidth: number;
+        glHeight: number;
+        option?: ApplicationOptions;
+    }): void;
+    /**
+     * フルスクリーンに切り替える
+     */
+    static requestFullScreen(): void;
+    /**
+     * 可能であれば新しいシーンへのトランジションを開始する
+     */
+    static transitionInIfPossible(newScene: Scene): boolean;
+    /**
+     * シーンをロードする
+     * 新しいシーンのリソース読み込みと古いシーンのトランジションを同時に開始する
+     * いずれも完了したら、新しいシーンのトランジションを開始する
+     */
+    static loadScene(newScene: Scene): void;
+    /**
+     * HTML canvas のりサイズ処理を行う
+     */
+    static resizeCanvas(): void;
+    /**
+     * 動作環境に応じて適切ならフルスクリーン設定をする
+     */
+    private static enableFullScreenIfNeeded;
+}
---
+++ dist/scenes/Scence.d.ts
@@ -0,0 +1,107 @@
+import * as PIXI from 'pixi.js';
+import LoaderAddParam from 'interfaces/LoaderAddParam';
+import Transition from 'interfaces/Transition';
+import UpdateObject from 'interfaces/UpdateObject';
+/**
+ * ゲームシーンの抽象クラス
+ * UiGraph を利用して UI 情報を透過的に読み込み初期化する
+ * また、シーン間のトランジションイベントを提供する
+ * いずれのイベントも実装クラスにて独自処理の実装を行うことができる
+ */
+export default abstract class Scene extends PIXI.Container {
+    /**
+     * 経過フレーム数
+     */
+    protected elapsedFrameCount: number;
+    /**
+     * UiGraph を利用して読み込む UI があるかどうか
+     */
+    protected hasSceneUiGraph: boolean;
+    /**
+     * UiGraph でロードされた UI データ
+     */
+    protected uiGraph: {
+        [key: string]: PIXI.Container;
+    };
+    /**
+     * UiGraph でロードされた UI データを配置するための PIXI.Container
+     * 描画順による前後関係を統制するために一つの Container にまとめる
+     */
+    protected uiGraphContainer: PIXI.Container;
+    /**
+     * 更新すべきオブジェクトを保持する
+     */
+    protected objectsToUpdate: UpdateObject[];
+    /**
+     * シーン開始用のトランジションオブジェクト
+     */
+    protected transitionIn: Transition;
+    /**
+     * シーン終了用のトランジションオブジェクト
+     */
+    protected transitionOut: Transition;
+    /**
+     * GameManager によって requestAnimationFrame 毎に呼び出されるメソッド
+     */
+    update(delta: number): void;
+    /**
+     * 更新処理を行うべきオブジェクトとして渡されたオブジェクトを登録する
+     */
+    protected registerUpdatingObject(object: UpdateObject): void;
+    /**
+     * 更新処理を行うべきオブジェクトを更新する
+     */
+    protected updateRegisteredObjects(delta: number): void;
+    /**
+     * シーン追加トランジション開始
+     * 引数でトランジション終了時のコールバックを指定できる
+     */
+    beginTransitionIn(onTransitionFinished: (scene: Scene) => void): void;
+    /**
+     * シーン削除トランジション開始
+     * 引数でトランジション終了時のコールバックを指定できる
+     */
+    beginTransitionOut(onTransitionFinished: (scene: Scene) => void): void;
+    /**
+     * loadInitialResource に用いるリソースリストを作成するメソッド
+     * デフォルトでは UiGraph のリソースリストを作成する
+     */
+    protected createInitialResourceList(): (LoaderAddParam | string)[];
+    /**
+     * リソースダウンロードのフローを実行する
+     * デフォルトでは UiGraph 用の情報が取得される
+     */
+    beginLoadResource(onLoaded: () => void): Promise<void>;
+    /**
+     * リソースダウンロードのフローを実行する
+     * UiGraph 情報も含まれる
+     */
+    protected loadInitialResource(onLoaded: () => void): void;
+    /**
+     * loadInitialResource 完了時のコールバックメソッド
+     */
+    protected onInitialResourceLoaded(): (LoaderAddParam | string)[];
+    /**
+     * 初回リソースロードで発生した追加のリソースをロードする
+     */
+    protected loadAdditionalResource(assets: (LoaderAddParam | string)[], onLoaded: () => void): void;
+    /**
+     * 追加のリソースロード完了時のコールバック
+     */
+    protected onAdditionalResourceLoaded(onLoaded: () => void): void;
+    /**
+     * beginLoadResource 完了時のコールバックメソッド
+     */
+    protected onResourceLoaded(): void;
+    /**
+     * UiGraph 用の PIXI.Container インスタンスに UiGraph 要素をロードする
+     */
+    /**
+     * UiGraph にシーン独自の要素を追加する場合にこのメソッドを利用する
+     */
+    protected getCustomUiGraphFactory(_type: string): null;
+    /**
+     * 渡されたアセットのリストからロード済みのものをフィルタリングする
+     */
+    private filterLoadedAssets;
+}
